3cadec5a76184056d1647eac3918a15af46db143
Viðar Ingi Ottósson <vidaringi0217@gmail.com>
2017-12-12 20:55:07 -0800

    Change frame type constants to hex

--- test/quic/frame/frame_types_test.exs
+++ test/quic/frame/frame_types_test.exs
@@ -7,27 +7,27 @@ defmodule QUIC.Frame.TypeTest do
 
   test "Stream frame type values" do
     ## No additional bits set
-    assert Type.stream(false, false, false) == 16
+    assert Type.stream(false, false, false) == 0x10
 
     ## FIN bit set
-    assert Type.stream(true, false, false) == 17
+    assert Type.stream(true, false, false) == 0x11
 
     ## LEN bit set
-    assert Type.stream(false, true, false) == 18
+    assert Type.stream(false, true, false) == 0x12
 
     ## FIN and LEN bits set
-    assert Type.stream(true, true, false) == 19
+    assert Type.stream(true, true, false) == 0x13
 
     ## OFF bit set
-    assert Type.stream(false, false, true) == 20
+    assert Type.stream(false, false, true) == 0x14
 
     ## FIN and OFF bits set
-    assert Type.stream(true, false, true) == 21
+    assert Type.stream(true, false, true) == 0x15
 
     ## LEN and OFF bit set
-    assert Type.stream(false, true, true) == 22
+    assert Type.stream(false, true, true) == 0x16
 
     ## FIN, LEN and OFF bits set
-    assert Type.stream(true, true, true) == 23
+    assert Type.stream(true, true, true) == 0x17
   end
 end
